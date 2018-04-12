df6874e1a8724c958fdc35734ddf558608d5db36
Steven Novak <snovak@incomm.com>
2018-04-12 16:48:00 -0700

    Added SIMD vector types

--- Math/SIMD.h
+++ Math/SIMD.h
@@ -12,6 +12,76 @@
 #ifndef SIMD_H
 #define SIMD_H
 
+// --------------------------------------------------
+// ------------------------- Structs
+
+typedef struct
+{
+    float x;
+    float y;
+} float2;
+
+typedef struct
+{
+    float x;
+    float y;
+    float z;
+} float3;
+
+typedef struct
+{
+    float x;
+    float y;
+    float z;
+    float w;
+} float4;
+
+typedef struct
+{
+    double x;
+    double y;
+} double2;
+
+typedef struct
+{
+    double x;
+    double y;
+    double z;
+} double3;
+
+typedef struct
+{
+    double x;
+    double y;
+    double z;
+    double w;
+} double4;
+
+typedef struct
+{
+    int x;
+    int y;
+} int2;
+
+typedef struct
+{
+    int x;
+    int y;
+    int z;
+} int3;
+
+typedef struct
+{
+    int x;
+    int y;
+    int z;
+    int w;
+} int4;
+
+
+// --------------------------------------------------
+// ------------------------- Functions
+
 
 
 #endif
