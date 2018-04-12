582509c30062c4e5a1e71605f7f6b4e2fc78138b
Steven Novak <stevenrnovak@gmail.com>
2018-04-12 08:18:07 -0700

    Basic utils and test files

---
+++ Utils/CPUInfo.h
@@ -0,0 +1,17 @@
+// --------------------------------------------------
+// CPUInfo.h  
+// 
+// Routines to determine CPU Capabilities
+//
+// Steven Novak
+//
+// 04/12/2018
+//
+// --------------------------------------------------
+
+#ifndef CPUINFO_H
+#define CPUINFO_H
+
+
+
+#endif
---
+++ Utils/Test/CPUInfo.test.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+int main(int argCount, char** args)
+{
+
+    return 0;
+}
