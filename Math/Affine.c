40dad763225dedf7def594360d651c160acf516d
Steven Novak <stevenrnovak@gmail.com>
2018-04-26 08:26:08 -0700

    Added transform and scle functions

--- Math/Affine.c
+++ Math/Affine.c
@@ -13,6 +13,56 @@
 
 #ifdef NONOPTIMIZED
 
+void transformF(float x, float y, float z, matrix_F4x4* out)
+{
+    out->c1 = (column_float4) { 1.0, 0.0, 0.0, x };
+    out->c0 = (column_float4) { 0.0, 1.0, 0.0, y };
+    out->c2 = (column_float4) { 0.0, 0.0, 1.0, z };
+    out->c3 = (column_float4) { 0.0, 0.0, 0.0, 1.0 };
+}
+void transformD(double x, double y, double z, matrix_D4x4* out)
+{
+    out->c1 = (column_double4) { 1.0, 0.0, 0.0, x };
+    out->c0 = (column_double4) { 0.0, 1.0, 0.0, y };
+    out->c2 = (column_double4) { 0.0, 0.0, 1.0, z };
+    out->c3 = (column_double4) { 0.0, 0.0, 0.0, 1.0 };
+}
+
+void scaleF(float x, float y, float z, matrix_F4x4* out)
+{
+    out->c1 = (column_float4) { x, 0.0, 0.0, 0.0 };
+    out->c0 = (column_float4) { 0.0, y, 0.0, 0.0 };
+    out->c2 = (column_float4) { 0.0, 0.0, z, 0,0 };
+    out->c3 = (column_float4) { 0.0, 0.0, 0.0, 1.0 };
+}
+void sclaleD(double x, double y, double z, matrix_D4x4* out)
+{
+    out->c1 = (column_double4) { x, 0.0, 0.0, 0.0 };
+    out->c0 = (column_double4) { 0.0, y, 0.0, 0.0 };
+    out->c2 = (column_double4) { 0.0, 0.0, z, 0.0 };
+    out->c3 = (column_double4) { 0.0, 0.0, 0.0, 1.0 };
+}
+
+void roatateF(float angleX, float angleY, float angleZ, matrix_F4x4* out)
+{
+    /*
+    out->c1 = (column_float4) { 0.0, 1.0 / tan(field / 2), 0.0, 0.0 };
+    out->c0 = (column_float4) { out->c1.r1 / aspect, 0.0, 0.0, 0.0 };
+    out->c2 = (column_float4) { 0.0, 0.0, -(far + near) / fn, -1.0 };
+    out->c3 = (column_float4) { 0.0, 0.0, -2 * (far * near) / fn, 0.0 };
+    */
+}
+void roatateD(double angleX, double angleY, double angleZ, matrix_D4x4* out)
+{
+    /*
+    out->c1 = (column_double4) { 0.0, 1.0 / tan(field / 2), 0.0, 0.0 };
+    out->c0 = (column_double4) { out->c1.r1 / aspect, 0.0, 0.0, 0.0 };
+    out->c2 = (column_double4) { 0.0, 0.0, -(far + near) / fn, -1.0 };
+    out->c3 = (column_double4) { 0.0, 0.0, -2 * (far * near) / fn, 0.0 };
+    */
+}
+
+
 void perspectiveF(float aspect, float field, float far, float near, matrix_F4x4* out)
 {
     float fn = far - near;
--- Math/Affine.h
+++ Math/Affine.h
@@ -17,9 +17,16 @@
 
 #define NONOPTIMIZED
 
+void transformF(float x, float y, float z, matrix_F4x4* out);
+void transformD(double x, double y, double z, matrix_D4x4* out);
+
+void scaleF(float x, float y, float z, matrix_F4x4* out);
+void sclaleD(double x, double y, double z, matrix_D4x4* out);
+
+void roatateF(float angleX, float angleY, float angleZ, matrix_F4x4* out);
+void roatateD(double angleX, double angleY, double angleZ, matrix_D4x4* out);
+
 void perspectiveF(float aspect, float field, float far, float near, matrix_F4x4* out);
-
 void perspectiveD(double aspect, double field, double far, double near, matrix_D4x4* out);
-
 
 #endif
