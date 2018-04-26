ee82513e67cf48480a6d78a64444e85f53aabd57
Steven Novak <stevenrnovak@gmail.com>
2018-04-26 00:30:02 -0700

    Added perspective matrix to affline transforms. Needs tests

--- Math/Affline.c
+++ Math/Affline.c
@@ -8,3 +8,29 @@
 // 04/25/2018
 //
 // --------------------------------------------------
+
+#include "Affline.h"
+
+#ifdef NONOPTIMIZED
+
+void perspectiveF(float aspect, float field, float far, float near, matrix_F4x4* out)
+{
+    float fn = far - near;
+    out->c1 = (column_float4) { 0.0, 1.0 / tan(field / 2), 0.0, 0.0 };
+    out->c0 = (column_float4) { out->c1.r1 / aspect, 0.0, 0.0, 0.0 };
+    out->c2 = (column_float4) { 0.0, 0.0, -(far + near) / fn, -1.0 };
+    out->c3 = (column_float4) { 0.0, 0.0, -2 * (far * near) / fn, 0.0 };
+}
+
+void perspectiveD(double aspect, double field, double far, double near, matrix_D4x4* out)
+{
+    double fn = far - near;
+    out->c1 = (column_double4) { 0.0, 1.0 / tan(field / 2), 0.0, 0.0 };
+    out->c0 = (column_double4) { out->c1.r1 / aspect, 0.0, 0.0, 0.0 };
+    out->c2 = (column_double4) { 0.0, 0.0, -(far + near) / fn, -1.0 };
+    out->c3 = (column_double4) { 0.0, 0.0, -2 * (far * near) / fn, 0.0 };
+}
+
+#else
+
+#endif
--- Math/Affline.h
+++ Math/Affline.h
@@ -8,3 +8,18 @@
 // 04/25/2018
 //
 // --------------------------------------------------
+
+#ifndef AFFLINE_H
+#define AFFLINE_H
+
+#include "Primatives.h"
+
+#define NONOPTIMIZED
+
+void perspectiveF(float aspect, float field, float far, float near, matrix_F4x4* out);
+
+
+void perspectiveD(double aspect, double field, double far, double near, matrix_D4x4* out);
+
+
+#endif
