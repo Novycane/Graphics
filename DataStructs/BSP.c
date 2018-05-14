d2dd0a401c7f7487628ed55b8ca2fac2c427a476
credential.helper <33043784+SNovakInComm@users.noreply.github.com>
2018-05-14 13:25:05 -0700

    Added some comments and function declarations to BSP Compiler

--- DataStructs/BSP.c
+++ DataStructs/BSP.c
@@ -11,5 +11,21 @@
 
 #include "BSP.h"
 
+// -------------------------------------------------- Declarations
+
+BSPNode* createNode(BSPNode* parent);
+int isInFront();
+
 // -------------------------------------------------- BSP
 
+int createBSPTreeFromVertexBuffer(float4* vertexBuffer, unsigned int count)
+{
+    // Are the remaining polygons a convex set?
+    // - Yes -> return
+
+    // Find Center Of Poly Set
+    // Create Node
+    // Assign center to node
+
+    return 1;
+}
--- DataStructs/BSP.h
+++ DataStructs/BSP.h
@@ -12,8 +12,29 @@
 #ifndef BSP_H
 #define BSP_H
 
+#include "../Math/Polygon.h"
+
+// -------------------------------------------------- Structs
+typedef enum 
+{
+    isSolid = 1,
+    isLeaf = 2
+} BSPFlags;
+
+typedef struct  BSPNode  BSPNode;
+
+struct BSPNode
+{
+    BSPNode* parent;
+    BSPNode* front;
+    BSPNode* back;
+    TriangleF3* faces;
+    unsigned int numberOfFaces;
+    BSPFlags flags;
+};
+
 // -------------------------------------------------- BSP
 
-
+int createBSPTreeFromVertexBuffer(float4* vertexBuffer, unsigned int count);
 
 #endif
